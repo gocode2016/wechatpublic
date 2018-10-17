93fe092312b188e691a5b187b1e53cd415c72119
turnipbun <turnipbun@icloud.com>
2018-10-17 20:48:42 +0800

    draw a lottery

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import xml.etree.ElementTree as ET
 import re
 import time
 import random
+import urllib
+import json
+import threading
+import os
 app = Flask(__name__)
 
 g_xmlForm = '''
@@ -16,17 +20,49 @@ g_xmlForm = '''
 <Content><![CDATA[{Content}]]></Content>
 </xml>'''
 
+class TokenManager(threading.Thread):
+    '''
+    '''
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        while True:
+            self.resetToken()
+            time.sleep(7000)
+    def resetToken(self):
+        appid = os.getenv('APPID')
+        appsecret = os.getenv('APPSECRET')
+        url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s" % (appid, appsecret))
+        resp = urllib.urlopen(url)
+        jsonResp = json.loads(resp.read())
+        self.token = jsonResp['access_token']
+        print(self.token)
+    def getToken(self):
+        return self.token
+
+g_tokenManager = TokenManager()
+
 class Lottery(object):
     def __init__(self):
         self.userList = list()
     def addUser(self,userName):
-        if self.userList.cout(userName) == 0:
+        '''增加用户:判断用户是否已经存在,添加用户到list
+        '''
+        if self.userList.count(userName) == 0:
             self.userList.append(userName)
     def draw(self):
+        '''抽奖:产生随机下标,保存用户,重置,将用户返回
+        '''
         index = random.randint(0,len(self.userList)-1)
-        return self.userList[index]
-    def clear(self):
+        luckyUser = self.userList[index]
+        self.reset()
+        return luckyUser
+    def reset(self):
         del self.userList[:]
+    def count(self):
+        return len(self.userList)
+
+g_lottery = Lottery()
 
 class Msg(object):
     def __init__(self):
@@ -45,10 +81,9 @@ class TextMsg(Msg):
         match = pattern.search(self.content)
         if match:
             self.define = match.group()
-            print("It's a defined msg")
             return True
         else:
-            print("It's a undefined msg")
+            print("It's an undefined msg")
             return False
     def reply(self):
         if self.isDefined():
@@ -57,10 +92,11 @@ class TextMsg(Msg):
                 msgDict['ToUserName'] = self.msgHead.getFromUserName()
                 msgDict['FromUserName'] = self.msgHead.getToUserName()
                 msgDict['CreateTime'] = int(time.time())
-                msgDict['Content'] = '感谢您的参与,我们将在稍后开奖'
+                g_lottery.addUser(msgDict['ToUserName'])
+                msgDict['Content'] = '感谢参与,目前共有%s人,我们将在稍后开奖' % g_lottery.count()
+                print(msgDict['Content'])
                 return g_xmlForm.format(**msgDict)
         return 'success'
-        
 
 class MsgHead(object):
     def __init__(self,xmlData,):
@@ -84,6 +120,13 @@ class MsgHead(object):
         if self.msgType == 'text':
             return TextMsg(self.xmlData.find('Content'),self)
 
+@app.route("/draw_lottery")
+def drawLottery():
+    if g_lottery.count() > 0:
+        return g_lottery.draw()
+    else:
+        return 'no participator'
+        
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -93,7 +136,7 @@ def wx_post():
     data = request.data
     msgHead = MsgHead(ET.fromstring(data))
     msg = msgHead.getMsg()
-    return msg.reply();
+    return msg.reply()
     
 @app.route("/wx", methods=['GET'])
 def handle():
@@ -122,4 +165,5 @@ def handle():
         return echostr
     
 if __name__ == '__main__':
+    g_tokenManager.start()
     app.run(host='0.0.0.0',port=80)
--- test_main.py
+++ test_main.py
@@ -13,13 +13,25 @@ def client(request):
     return client
 
 def test_hello(client):
-    rv = client.get("/");
+    rv = client.get('/');
     assert b'Hello World' in rv.data
 
 def test_msg(client):
-    rv = client.post("/wx?signature=7895274204367b506b5c27ed540972c3552a5e53&timestamp=1539091653&nonce=1841256615&openid=oTwo10dnoDY35gK5APNGWfdPzuU0",data='<xml><ToUserName><![CDATA[gh_0f708d1d8ace]]></ToUserName>\n<FromUserName><![CDATA[oTwo10dnoDY35gK5APNGWfdPzuU0]]></FromUserName>\n<CreateTime>1539091653</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[\xe4\xbd\xa0\xe5\xa5\xbd]]></Content>\n<MsgId>6610348315604941314</MsgId>\n</xml>')
+    rv = client.post('/wx?signature=7895274204367b506b5c27ed540972c3552a5e53&timestamp=1539091653&nonce=1841256615&openid=oTwo10dnoDY35gK5APNGWfdPzuU0',data='<xml><ToUserName><![CDATA[gh_0f708d1d8ace]]></ToUserName>\n<FromUserName><![CDATA[oTwo10dnoDY35gK5APNGWfdPzuU0]]></FromUserName>\n<CreateTime>1539091653</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[\xe4\xbd\xa0\xe5\xa5\xbd]]></Content>\n<MsgId>6610348315604941314</MsgId>\n</xml>')
     assert b'success' in rv.data
 
-def test_msg_cj(client):
-    rv = client.post("/wx?signature=7895274204367b506b5c27ed540972c3552a5e53&timestamp=1539091653&nonce=1841256615&openid=oTwo10dnoDY35gK5APNGWfdPzuU0",data='<xml><ToUserName><![CDATA[gh_0f708d1d8ace]]></ToUserName>\n<FromUserName><![CDATA[oTwo10dnoDY35gK5APNGWfdPzuU0]]></FromUserName>\n<CreateTime>1539091653</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[[CJ]\xe4\xbd\xa0\xe5\xa5\xbd]]></Content>\n<MsgId>6610348315604941314</MsgId>\n</xml>')
+def test_msg_cj1(client):
+    rv = client.post('/wx?signature=7895274204367b506b5c27ed540972c3552a5e53&timestamp=1539091653&nonce=1841256615&openid=oTwo10dnoDY35gK5APNGWfdPzuU0',data='<xml><ToUserName><![CDATA[gh_0f708d1d8ace]]></ToUserName>\n<FromUserName><![CDATA[oTwo10dnoDY35gK5APNGWfdPzuU0]]></FromUserName>\n<CreateTime>1539091653</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[[CJ]\xe4\xbd\xa0\xe5\xa5\xbd]]></Content>\n<MsgId>6610348315604941314</MsgId>\n</xml>')
     assert b'\xe6\x88\x91\xe4\xbb\xac\xe5\xb0\x86\xe5\x9c\xa8\xe7\xa8\x8d\xe5\x90\x8e\xe5\xbc\x80\xe5\xa5\x96' in rv.data
+
+def test_msg_cj2(client):
+    rv = client.post('/wx?signature=7895274204367b506b5c27ed540972c3552a5e53&timestamp=1539091653&nonce=1841256615&openid=oTwo10dnoDY35gK5APNGWfdPzuU0',data='<xml><ToUserName><![CDATA[gh_0f708d1d8ace]]></ToUserName>\n<FromUserName><![CDATA[Towo10dnoDY35gK5APNGWfdPzuU0]]></FromUserName>\n<CreateTime>1539091653</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[[CJ]\xe4\xbd\xa0\xe5\xa5\xbd]]></Content>\n<MsgId>6610348315604941314</MsgId>\n</xml>')
+    assert b'\xe6\x88\x91\xe4\xbb\xac\xe5\xb0\x86\xe5\x9c\xa8\xe7\xa8\x8d\xe5\x90\x8e\xe5\xbc\x80\xe5\xa5\x96' in rv.data
+
+def test_draw_lottery(client):
+    rv = client.get('/draw_lottery')
+    assert len(rv.data) == 28
+
+def test_draw_lottery(client):
+    rv = client.get('/draw_lottery')
+    assert b'no' in rv.data
